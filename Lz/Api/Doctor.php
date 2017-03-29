68410350701da83e0cf4070ef12ee2e2976727de
Mrlaozhou <zgs5999@163.com>
2017-03-29 13:31:13 +0800

    add api order person

--- Lz/Api/Doctor.php
+++ Lz/Api/Doctor.php
@@ -27,14 +27,14 @@ class Doctor extends Base
 	{
 		/*需求介绍*///提取单条 http://www.mzxznz.cn/api.php?u=eW52Z3JxXm4mZWJncGJxXno%3D
 		$id = P('id');
+		// $id = 100;
 		if( $id )
 		{
 			$doctor = M('doctor');
 			$sql = "SELECT d.id,d.name,d.title,d.edu,d.sex,d.name,d.picture,d.duty,d.good,d.intro,d.pass,d.start,(h.name)hos_name,(h.intro)hos_intro FROM 
 						zxznz_doctor AS d 
 						LEFT JOIN zxznz_hospital AS h 
-						ON d.hos_id = h.id 
-						WHERE d.is_show = '是' 
+						ON d.hos_id = h.id  
 						AND h.is_show = '1' 
 						AND d.id = {$id} ";
 			$info = $doctor->One($sql);
@@ -51,7 +51,7 @@ class Doctor extends Base
 		}
 		else
 		{
-			echoJson(array('result'=>FALSE));
+			echoJson(array('status'=>FALSE));
 		}
 	}
 }
