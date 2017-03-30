9538e46d6dd2f965a03f66e9b2bf518e950772bd
Mrlaozhou <zgs5999@163.com>
2017-03-30 11:15:07 +0800

    null

--- Lz/Api/Order.php
+++ Lz/Api/Order.php
@@ -2,32 +2,27 @@
 
 class Order extends Allow
 {
-	public function yes()
+	public function self()
 	{
-		/*需求介绍*///提取已支付订单 ZnJsXm4mZXJxZWJeeg%3D%3D
+		/*需求介绍*///提取已支付订单 c3lyZl5uJmVycWViXno%3D
 		$model = M('Order');
 		$user_id = S('USER_ID');
 		$sql = "SELECT o.id,o.status,o.code,o.active_id,o.create_time,o.need_pay,o.true_pay,o.count,a.title,a.pic,a.intro,a.hospital FROM 
 						`zxznz_order` AS o 
 						LEFT JOIN `zxznz_active` AS a ON a.id = o.active_id 
 						LEFT JOIN `zxznz_user` AS u ON u.id = o.user_id 
-						WHERE o.user_id = {$user_id} 
-						AND o.status = '2'";
+						WHERE o.user_id = {$user_id}";
 		$data = $model->All($sql);
-		echoJson($data);
+		$result = array();
+		foreach( $data as $k => $v )
+		{
+			if( $v['status'] == '2' )
+				$result['yes'] = $v;
+			elseif( $v['status'] == '1' )
+				$result['no'] = $v;
+			unset($data[$k]);
+		}
+		echoJson($result);
 	}
-	public function no()
-	{
-		/*需求介绍*///提取未支付订单 YmFebiZlcnFlYl56
-		$model = M('Order');
-		$user_id = S('USER_ID');
-		$sql = "SELECT o.id,o.status,o.code,o.active_id,o.create_time,o.need_pay,o.true_pay,o.count,a.title,a.pic,a.intro,a.hospital FROM 
-						`zxznz_order` AS o 
-						LEFT JOIN `zxznz_active` AS a ON a.id = o.active_id 
-						LEFT JOIN `zxznz_user` AS u ON u.id = o.user_id 
-						WHERE o.user_id = {$user_id} 
-						AND o.status = '1'";
-		$data = $model->All($sql);
-		echoJson($data);
-	}
+
 }
