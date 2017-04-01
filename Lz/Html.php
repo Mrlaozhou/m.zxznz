831988b5e2d9a7b90541169d013d2c8e1f286e36
Mrlaozhou <zgs5999@163.com>
2017-04-01 17:33:28 +0800

    use 404

--- Lz/Html.php
+++ Lz/Html.php
@@ -14,22 +14,30 @@ defined('VIEW_PATH') or define('VIEW_PATH',HTML_PATH.'view'.DS);
 defined('CONTROLLER') or define('CONTROLLER',isset($_GET['c']) ? ucwords(trim($_GET['c'])) : null);
 defined('ACTION') or define('ACTION',isset($_GET['a']) ? trim($_GET['a']) : 'index');
 
-
 //加载函数文件
 find('./Config/functions.php');
+
 
 if( CREATE )
 	require_once('./Lz/Base/InitMake.php');
 if( CONTROLLER )
 	find(SRC_PATH.CONTROLLER.'.php');
 if( ACTION )
+{
+	$views = scandir(VIEW_PATH);
+	array_shift($views);
+	array_shift($views);
+	$a = ACTION.'.html';
+	if( !in_array($a,$views) )
+		//页面错误拦截
+		find(VIEW_PATH.'404.html');
 	find(VIEW_PATH.ACTION.'.html');
+}
 
 
 function find($path)
 {
 	if( file_exists($path) )
 		require_once($path);
-	else
-		echo $path.'不存在！';
+	return ;
 }
