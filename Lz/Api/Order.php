86547e47e99941ffbaaae0f7ef968f9de4f9b35c
Mrlaozhou <zgs5999@163.com>
2017-04-07 17:03:52 +0800

    add ali

--- Lz/Api/Order.php
+++ Lz/Api/Order.php
@@ -120,6 +120,7 @@ class Order extends Allow
 	public function pay()
 	{
 		/*需求介绍*///订单支付  bG5jXm4mZXJxZWJeeg%3D%3D 
+
 		if( !P('pay_type') || !P('orderId') )
 			echoJson(array('status'=>FALSE,'info'=>'001'));
 		//取出订单信息
@@ -130,14 +131,15 @@ class Order extends Allow
 								WHERE o.id = {$orderId} 
 								LIMIT 1";
 		$info = M('order')->One($sql);
-
+		
 		if( !$info )
 			echoJson(array('status'=>FALSE,'info'=>'006'));
-
+		// dump(P('pay_type'));
 		switch (P('pay_type')) 
 		{
 			case '1':
 				# 支付宝
+				echo 1;
 				$this->aliPay($info);
 				break;
 			case '2':
@@ -154,58 +156,60 @@ class Order extends Allow
 		/*需求介绍*///阿里支付接口 bG5DdnluXm4mZXJxZWJeeg%3D%3D
 		//定义路径常量
 		define('ALI',VENDOR_PATH.'Alipay'.DS);
-		define('AOP_SDK_WORK_DIR',ALI.'tmp/');
 
-		//手动加载类
-		load(ALI.'AlipayTradeService.php');
-		load(ALI.'AlipayTradeWapPayContentBuilder.php');
-		$config = load(ALI.'config.php');
+		$alipay_config = load(ALI.'alipay.config.php');
+		load(ALI.'alipay_submit.class.php');
 
-	    //商户订单号，商户网站订单系统中唯一订单号，必填
-	    $out_trade_no = $info['id'];
 
-	    //订单名称，必填
-	    $subject = $info['title'];
 
-	    //付款金额，必填
-	    $total_amount = $info['need_pay'];
+		/**请求参数**/
+        //商户订单号，商户网站订单系统中唯一订单号，必填
+        $out_trade_no = $info['id'];
 
-	    //商品描述，可空
-	    $body = $info['code'];
+        //订单名称，必填
+        $subject = $info['title'];
 
-	    //超时时间
-	    $timeout_express="1m";
+        //付款金额，必填
+        $total_fee = $info['need_pay'];
 
-	    $payRequestBuilder = new AlipayTradeWapPayContentBuilder();
-	    $payRequestBuilder->setBody($body);
-	    $payRequestBuilder->setSubject($subject);
-	    $payRequestBuilder->setOutTradeNo($out_trade_no);
-	    $payRequestBuilder->setTotalAmount($total_amount);
-	    $payRequestBuilder->setTimeExpress($timeout_express);
+        //商品描述，可空
+        $body = $info['code'];
 
-	    $payResponse = new AlipayTradeService($config);
-	    $result=$payResponse->wapPay($payRequestBuilder,$config['return_url'],$config['notify_url']);
-	    echoJosn(array('status'=>TRUE,'info'=>$result));
+		/***********/
+
+		//构造要请求的参数数组，无需改动
+		$parameter = array(
+				"service"       => $alipay_config['service'],
+				"partner"       => $alipay_config['partner'],
+				"seller_id"  => $alipay_config['seller_id'],
+				"payment_type"	=> $alipay_config['payment_type'],
+				"notify_url"	=> $alipay_config['notify_url'],
+				"return_url"	=> $alipay_config['return_url'],
+				
+				"anti_phishing_key"=>$alipay_config['anti_phishing_key'],
+				"exter_invoke_ip"=>$alipay_config['exter_invoke_ip'],
+				"out_trade_no"	=> $out_trade_no,
+				"subject"	=> $subject,
+				"total_fee"	=> $total_fee,
+				"body"	=> $body,
+				"_input_charset"	=> trim(strtolower($alipay_config['input_charset']))
+				//其他业务参数根据在线开发文档，添加参数.文档地址:https://doc.open.alipay.com/doc2/detail.htm?spm=a219a.7629140.0.0.kiX33I&treeId=62&articleId=103740&docType=1
+		        //如"参数名"=>"参数值"			
+		);
+
+		//建立请求
+		$alipaySubmit = new AlipaySubmit($alipay_config);
+		$html_text = $alipaySubmit->buildRequestForm($parameter,"get", "确认");
+		echo $html_text;
+
+		// dump($html_text);
 	}
 
 	public function aliGet($info=null)
 	{
 		/*需求介绍*///阿里回调接口 Z3JUdnluXm4mZXJxZWJeeg%3D%3D
 		//接收参数
-		if( $receive = P() )
-			echoJson(array('status'=>FALSE,'info'=>'001'));
-		define('ALI',VENDOR_PATH.'Alipay'.DS);
-		$config = load(ALI.'config.php');
-		load(ALI.'AlipayTradeService.php');
-
-		$alipaySevice = new AlipayTradeService($config); 
-		$alipaySevice->writeLog(var_export($receive,true));
-		$result = $alipaySevice->check($receive);
-
-		if( $result )
-		{
-
-		}
+		
 	}
 
 	public function wxPay()
