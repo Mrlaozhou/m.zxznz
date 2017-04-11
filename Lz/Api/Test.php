65c212b1626b8e6cb24fa9f559ea98fccd481c9d
Mrlaozhou <zgs5999@163.com>
2017-04-11 10:33:08 +0800

    add wxjs test

--- Lz/Api/Test.php
+++ Lz/Api/Test.php
@@ -4,6 +4,12 @@ class Test extends Base
 {
 	public function index()
 	{
-		
+		/********///a3JxYXZebiZnZnJHXno%3D
+
+		$url = "https://api.weixin.qq.com/cgi-bin/userinfo?";
+		$config = "access_token=z4EGkU-naDmXPk71BQQyy41FQDVzgxzsdqQMLSr309CgryTbbaZdvHuW4vM4Zz-hlvEy2OsiVnC5mwbHvaTpA4tVvm9fA32yA-E_WcUHrsY&openid=owt34v86zgSTrvdjHwSzHcwl2CNU&lang=zh_CN";
+
+		$str = Post($config,$url);;
+		dump($str);
 	}
 }
